de951e645a9f98a443e98de3368c85f8b7299c5d
Michael Mairegger <michael.mairegger@schwer.bz>
2018-05-17 16:47:33 +0200

    Continued with VSCode extension

--- codecallfrequency/src/extension.ts
+++ codecallfrequency/src/extension.ts
@@ -2,11 +2,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as http from 'http';
+import { log } from 'util';
 
-class CallStackCommand implements vscode.Command
-{
-    constructor(title: string, command:string)
-    {
+class CallStackCommand implements vscode.Command {
+    constructor(title: string, command: string) {
         this.title = title;
         this.command = command;
     }
@@ -15,13 +15,11 @@ class CallStackCommand implements vscode.Command
     command: string;
 }
 
-class CallStackLens extends vscode.CodeLens
-{
-    method:string;
+class CallStackLens extends vscode.CodeLens {
+    method: string;
     calls: number;
 
-    constructor(range:vscode.Range, method: string)
-    {
+    constructor(range: vscode.Range, method: string) {
         super(range);
 
         this.method = method;
@@ -31,42 +29,67 @@ class CallStackLens extends vscode.CodeLens
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-class CustomCodeLensProvider implements vscode.CodeLensProvider
-{
-    provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.CodeLens[] | Thenable<vscode.CodeLens[]>
-    {
+class CustomCodeLensProvider implements vscode.CodeLensProvider {
+    provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.CodeLens[] | Thenable<vscode.CodeLens[]> {
         //(?:public|protected|internal|private)(\s+async){0,1}(?:\s+(?:abstract|static|virtual|override|sealed))?(?:\s+)(?<returnValue>\w+(?:<(?<genericReturnArguments>(?:\w+(?:\s*\,\s*)?)*)>)?)(?:\s+)(?<methodName>\w+)(?:<(?<genericMethodArguments>(?:\w+(?:\s*\,\s*)?)*)>)?\s*\((?<parameters>(?:\w+(?:<(?<generic3>(?:\w+(?:\s*\,\s*)?)*)>)?\s+\w*(?:\s*\,\s*)?)*)\)
         const regEx = /(?:public|protected|internal|private)(\s+async){0,1}(?:\s+(?:abstract|static|virtual|override|sealed))?(?:\s+)(\w+(?:<((?:\w+(?:\s*\,\s*)?)*)>)?)(?:\s+)(\w+)(?:<((?:\w+(?:\s*\,\s*)?)*)>)?\s*\(((?:\w+(?:<((?:\w+(?:\s*\,\s*)?)*)>)?\s+\w*(?:\s*\,\s*)?)*)\)/g;
         const text = document.getText();
         let match;
 
         let ret: vscode.CodeLens[] = [];
-        
-        while (match = regEx.exec(text)) 
-        {
+
+        while (match = regEx.exec(text)) {
             const startPos = document.positionAt(match.index);
             const endPos = document.positionAt(match.index + match[0].length);
-            
+
             ret.push(new CallStackLens(new vscode.Range(startPos, endPos), match[0]));
         }
 
         return ret;
     }
 
-    resolveCodeLens(codeLens: vscode.CodeLens, token: vscode.CancellationToken): vscode.CodeLens | Thenable<vscode.CodeLens> 
-    {
-        if(codeLens instanceof CallStackLens)
-        {
+    resolveCodeLens(codeLens: vscode.CodeLens, token: vscode.CancellationToken): vscode.CodeLens | Thenable<vscode.CodeLens> {
+        if (codeLens instanceof CallStackLens && codeLens) {
             var callStackLens = codeLens as CallStackLens;
-            callStackLens.command = new CallStackCommand("last 24h: "+ callStackLens.method.length + " calls","");
+            callStackLens.command = new CallStackCommand("computing code calls", "");
 
-            return callStackLens;
+            return new Promise<vscode.CodeLens>((resolve, reject) => {
+                http.get(
+                    "http://192.168.43.61:4000/api/read?predicate=1&subject=" + codeLens.method,
+                    res => {
+                        let content: string = "";
+
+                        res.addListener("readable", () => {
+                            var line = res.read();
+                            if (line) {
+                                content += line;
+                            }
+                        });
+
+
+                        res.addListener("end",
+                            () => {
+                                if (codeLens.command) {
+                                    try {
+                                        var json = JSON.parse(content);
+                                        log(codeLens.method);
+                                        codeLens.command.title = json.result + " calls last 24 hours";
+                                        resolve(codeLens);
+                                        //this.onDidChangeCodeLenses()
+                                    }
+                                    catch (e) {
+                                        log(e);
+                                    }
+                                }
+                            });
+                    });
+            });
+
         }
-        return codeLens;
+        return Promise.reject<vscode.CodeLens>(codeLens);
     }
 }
 
-export function activate(ctx: vscode.ExtensionContext):void
-{
-    ctx.subscriptions.push(vscode.languages.registerCodeLensProvider(['*'],new CustomCodeLensProvider()));
+export function activate(ctx: vscode.ExtensionContext): void {
+    ctx.subscriptions.push(vscode.languages.registerCodeLensProvider(['*'], new CustomCodeLensProvider()));
 }
